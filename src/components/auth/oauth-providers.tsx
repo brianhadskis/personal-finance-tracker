4fe7e8652d2687a503dbda8ca051275c45068628
Brian <brian.hadskis@hotmail.com>
2024-02-18 00:15:36 -0500

    Update styling for OAuthProviders component

--- src/components/auth/oauth-providers.tsx
+++ src/components/auth/oauth-providers.tsx
@@ -6,13 +6,27 @@ import { Button } from "../ui/button";
 
 export const OAuthProviders = () => {
   return (
-    <div className="flex flex-col items-center w-full gap-x-2">
-      <p>--- OR ---</p>
-      <Button size="lg" className="w-full" variant="outline" onClick={() => {}}>
-        <FcGoogle className="h-5 w-5" />
+    <div className="flex flex-col items-center w-full gap-y-2">
+      <p className="mb-2 flex flex-row items-center gap-x-3 text-neutral-500 text-sm">
+        <div className="h-[1px] w-[65px] bg-neutral-400" />
+        OR
+        <div className="h-[1px] w-[65px] bg-neutral-400" />
+      </p>
+      <Button
+        size="lg"
+        className="w-full gap-x-2 text-md font-semibold"
+        variant="outline"
+        onClick={() => {}}
+      >
+        <FcGoogle className="h-5 w-5" /> Google
       </Button>
-      <Button size="lg" className="w-full" variant="outline" onClick={() => {}}>
-        <FaGithub className="h-5 w-5" />
+      <Button
+        size="lg"
+        className="w-full gap-x-2 text-md font-semibold"
+        variant="outline"
+        onClick={() => {}}
+      >
+        <FaGithub className="h-5 w-5" /> Github
       </Button>
     </div>
   );
